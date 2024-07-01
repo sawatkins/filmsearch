cbfe2299c2b3d51df3dbf466e0f47dfa05e6c92b
sawatkins <siawatkins@gmail.com>
2024-06-30 23:17:17 -0700

    link to tmdb pages

--- handlers/search.go
+++ handlers/search.go
@@ -15,7 +15,6 @@ type Movie struct {
 	Title  string `json:"title"`
 	Year   int    `json:"year"`
 	Reason string `json:"justification"`
-	// Poster string `json:"poster"`
 }
 
 type Movies struct {
@@ -32,23 +31,21 @@ func Search(openaiClient *openai.Client, tmdbClient *tmdb.Client) fiber.Handler 
 		}
 
 		movieTitles, movieReasons := unmarshallMovieTitles(moviesJson)
-
-		// fmt.Println(movies.Movies[0].)
-
-		posters := getTmdbInfo(tmdbClient, movieTitles)
+		posters, tmdbUrls := getTmdbInfo(tmdbClient, movieTitles)
 
 		return c.Render("search", fiber.Map{
 			"Query":   query,
 			"Titles":  movieTitles,
 			"Posters": posters,
 			"Reasons": movieReasons,
-			// "Movies": movies.Movies,
+			"Urls":    tmdbUrls,
 		}, "layouts/main")
 	}
 }
 
-func getTmdbInfo(tmdbClient *tmdb.Client, movieTitles []string) []string {
+func getTmdbInfo(tmdbClient *tmdb.Client, movieTitles []string) ([]string, []string) {
 	var posters []string
+	var tmdbUrls []string
 	for _, movieTitle := range movieTitles {
 		parts := strings.Split(movieTitle, " (")
 		if len(parts) < 2 {
@@ -61,12 +58,15 @@ func getTmdbInfo(tmdbClient *tmdb.Client, movieTitles []string) []string {
 			"primary_release_year": year,
 		})
 		if err != nil {
-			log.Println(err)
+			log.Println("Error searching for movie:", err)
 			continue
 		}
 
-		fmt.Println(searchMovie.Results[0])
+		// get url
+		baseURL := "https://www.themoviedb.org/movie/"
+		tmdbUrls = append(tmdbUrls, fmt.Sprintf("%s%d", baseURL, searchMovie.Results[0].ID))
 
+		// get poster
 		if len(searchMovie.Results) > 0 || !strings.HasSuffix(searchMovie.Results[0].PosterPath, "jpg") {
 			posters = append(posters, tmdb.GetImageURL(searchMovie.Results[0].PosterPath, tmdb.W92))
 		} else {
@@ -74,8 +74,9 @@ func getTmdbInfo(tmdbClient *tmdb.Client, movieTitles []string) []string {
 			log.Println("No results found for movie:", movieTitle)
 		}
 	}
-	fmt.Println("posters:", posters)
-	return posters
+	fmt.Println("POSTERS:", posters)
+	fmt.Println("URLS:", tmdbUrls)
+	return posters, tmdbUrls
 }
 
 func unmarshallMovieTitles(data string) ([]string, []string) {
@@ -83,19 +84,20 @@ func unmarshallMovieTitles(data string) ([]string, []string) {
 	err := json.Unmarshal([]byte(data), &movies)
 	if err != nil {
 		fmt.Println(err)
-		return nil, nil // is this the best way to handle errors?
+		return nil, nil // is this the best way to handle errors here?
 	}
 
 	var movieTitles []string
 	for _, movie := range movies.Movies {
 		movieTitles = append(movieTitles, fmt.Sprintf("%s (%d)", movie.Title, movie.Year))
 	}
-	fmt.Println(movieTitles)
 
 	var movieReasons []string
 	for _, movie := range movies.Movies {
 		movieReasons = append(movieReasons, movie.Reason)
 	}
-	fmt.Println(movieReasons)
+	
+	fmt.Println("TITLES:", movieTitles)
+	fmt.Println("REASONS:", movieReasons)
 	return movieTitles, movieReasons
 }
