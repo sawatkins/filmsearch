5ab73fe8decec07fcb0674e968f2ebad08e80a36
sawatkins <siawatkins@gmail.com>
2024-06-30 21:59:26 -0700

    fix flags not getting parsed error

--- main.go
+++ main.go
@@ -16,12 +16,6 @@ import (
 	"github.com/sawatkins/eureka-search/handlers"
 )
 
-type Config struct {
-	Port    string
-	Prefork bool
-	Dev     bool
-}
-
 func loadEnv() {
 	if err := godotenv.Load(); err != nil {
 		log.Println("Error loading .env file!")
@@ -29,26 +23,23 @@ func loadEnv() {
 }
 
 func main() {
+	port := flag.String("port", ":8080", "Port to listen on")
+	prefork := flag.Bool("prefork", false, "Enable prefork in Production")
+	dev := flag.Bool("dev", true, "Enable development mode")
 	flag.Parse()
-
-	config := Config{
-		Port:    *flag.String("port", ":8080", "Port to listen on"),
-		Prefork: *flag.Bool("prefork", false, "Enable prefork in Production"),
-		Dev:     *flag.Bool("dev", true, "Enable development mode"),
-	}
 
 	loadEnv()
 	openaiClient := openai.NewClient(os.Getenv("OPENAI_API_KEY"))
 	tmdbClient, _ := tmdb.Init(os.Getenv("TMDB_API_KEY"))
 
 	engine := html.New("./views", ".html")
-	if config.Dev {
+	if *dev {
 		engine.Reload(true)
 		engine.Debug(true)
 	}
 
 	app := fiber.New(fiber.Config{
-		Prefork: config.Prefork,
+		Prefork: *prefork,
 		Views:   engine,
 	})
 
@@ -62,6 +53,6 @@ func main() {
 	app.Get("/api/openai", handlers.Openai(openaiClient))
 	app.Use(handlers.NotFound)
 
-	log.Println("Server starting on port", config.Port)
-	log.Fatal(app.Listen(config.Port)) // default port: 8080
+	log.Println("Server starting on port", *port)
+	log.Fatal(app.Listen(*port)) // default port: 8080
 }
