ee6bbfb015756abdc22d0f6a330d4b6f596dc1a0
sawatkins <siawatkins@gmail.com>
2024-07-07 15:41:23 -0700

    no info found input handling

--- static/public/js/search-results.js
+++
@@ -1,9 +0,0 @@
-window.onload = function () {
-    console.log("loaded script.js");
-    document.querySelectorAll(".tmdb_info").forEach(element => {
-        if (element.href === "not found") {
-            element.textContent = "no external info found";
-            element.removeAttribute("href");
-        }
-    });
-}
