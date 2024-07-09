1132dca8b81cbb73dc6a88c057235a7812443941
sawatkins <sw@sawatkins.com>
2024-07-08 18:07:46 -0700

    meta changes

--- handlers/openai.go
+++ handlers/openai.go
@@ -8,7 +8,7 @@ import (
 )
 
 const MODEL = "gpt-4o" 
-const PROMPT = "You are search engine with very deep and extensive knowledge of movies. A user will provide text describing a movie they are trying to find. Your job is to find the user's movie based on their description. Respont in valid JSON with only the relevant movie titles, years they were released, and short 1-2 sentece justification of specific aspects of the movie that match the user's description. Use the format from this example: { \"movies\": [ {  \"title\": \"movie title 1\", \"year\": 0000, \"justification\": \"justification sentences for movie 1 goes here\"}, {\"title\": \"movie 2 year\",\"year\": 0000, \"justification\": \"justification sentences for movie 2 goes here\"}, ect...]}"
+const PROMPT = "You are search engine with very deep and extensive knowledge of movies. A user will provide text describing a movie they are trying to find. Your job is to find the user's movie based on their description. Respont in valid JSON with only the relevant movie titles, years they were released, and short 1-2 sentece justification of specific aspects of the movie that match the user's description. If there are no relevant movies, respond with \"{}\" only. Use the format from this example: { \"movies\": [ {  \"title\": \"movie title 1\", \"year\": 0000, \"justification\": \"justification sentences for movie 1 goes here\"}, {\"title\": \"movie 2 year\",\"year\": 0000, \"justification\": \"justification sentences for movie 2 goes here\"}, ect...]}"
 
 func openaiMovieCompletion(openaiClient *openai.Client, query string) (string, error) {
 	request := openai.ChatCompletionRequest{
